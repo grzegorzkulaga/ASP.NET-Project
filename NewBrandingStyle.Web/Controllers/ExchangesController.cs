e12f4b5255184ddd548927dbdbe064aa22285dbd
Grzegorz Kułaga <grzegorz.kulaga@interia.pl>
2020-10-11 18:30:10 +0200

    Lab1 -

---
+++ NewBrandingStyle.Web/Controllers/ExchangesController.cs
@@ -0,0 +1,31 @@
+﻿using ExchangeThings.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewBrandingStyle.Web.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        public IActionResult Show()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Show(ExchangesModel exchanges)
+        {
+            var viewModel = new ExchangesAddViewModel
+            {
+                NumberOfCharsInName = exchanges.Name.Length,
+                NumberOfCharsInDescription = exchanges.Description.Length,
+                IsHidden = !exchanges.IsVisible
+
+            };
+            return View("ExchangesAdded", viewModel);
+        }
+    }
+}
